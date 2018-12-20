c92b80823b7ff60ef8afea5cda580c7322a723f0
d.trafimau <dzmitry.trafimau@bglgroup.co.uk>
2018-12-20 19:42:29 +0300

    separate update function

--- src/js/index.js
+++ src/js/index.js
@@ -18,93 +18,104 @@ function getFrameY(frame_height, framePosition) {
   return -frame_height * Math.floor(framePosition / 9);
 }
 
+function updateKeys(santaPerson){
+    const halfOfSanta = santaPerson.width / 2;
+    let speed = lastKeybEvent.shiftKey ? 21 : 7;
+    let turn = 0;
+    if (lastKeybEvent.key === "ArrowRight") {
+        turn = 1;
+    } else if (lastKeybEvent.key === "ArrowLeft") {
+        turn = -1;
+    }
+    if (turn !== 0) {
+        speed *= turn;
+        santaPerson.scale.set(-turn, 1);
+        let rightCorellation = Math.min(santaPerson.x + speed, APP_WIDTH - halfOfSanta);
+        santaPerson.x = Math.max(rightCorellation, halfOfSanta);
+    }
+}
+
+function updateSantaAnimation(santaPerson){
+    let delta_santa = Date.now() - start_generation_time;
+    const santaFrame = (delta_santa / 1000 / SANTA_FRAMES_PER_SECOND - 1);
+    if (santaDiedTime !== 0) {
+        santaPerson.tilePosition.x = getFrameX(SANTA_WIDTH, Math.round(53 - santaFrame % 2));
+        santaPerson.tilePosition.y = getFrameY(SANTA_HEIGHT, Math.round(53 - santaFrame % 2));
+    } else {
+        santaPerson.tilePosition.x = getFrameX(SANTA_WIDTH, Math.round(santaFrame % 50));
+        santaPerson.tilePosition.y = getFrameY(SANTA_HEIGHT, Math.round(santaFrame % 50));
+    }
+}
+
+function updateAnimations(santaPerson, bricksContainer, background, delta){
+    for (const index in bricksContainer.children) {
+        const child = bricksContainer.children[index];
+        if (child.y > APP_HEIGHT) {
+            if (Math.abs(child.x - santaPerson.x) < 100) {
+                santaDiedTime = Date.now();
+
+            }
+            bricksContainer.children.splice(index, 1);
+        } else {
+            child.y += 1;
+            child.rotation += 0.05 * delta;
+        }
+    }
+    background.tilePosition.y += 1;
+}
+
+function updateBricks(bricksContainer){
+    const newBricks = generateBricks();
+    for (const key in newBricks) {
+        if (newBricks.hasOwnProperty(key)) {
+            const brick = newBricks[key];
+            bricksContainer.addChild(brick);
+        }
+    }
+}
+
+function updateGameStatus(){
+    deltaUdpate = Date.now() - timeOfLastPartialUpdate;
+    if (
+        startGameTimestamp &&
+        (deltaUdpate > 250 || timeOfLastPartialUpdate === 0)
+    ) {
+        // TODO add cheap checks here
+        const secondsLeft = Math.ceil(
+            GAME_COUNTDOWN_SECONDS + (startGameTimestamp - Date.now()) / 1000
+        );
+        if (secondsLeft <= 0 && timeOfLastPartialUpdate !== 0) {
+            finishGame();
+        } else {
+            const minutes = Math.floor(secondsLeft / 60);
+            const seconds = secondsLeft % 60;
+            document.getElementById("minutes").innerHTML = `${minutes}:`;
+            document.getElementById("seconds").innerHTML = `0${seconds}`.slice(
+                -2
+            );
+            timeOfLastPartialUpdate = Date.now();
+        }
+    }
+}
+
 function wrapUpdate(santaPerson, bricksContainer, background) {
     return function update(delta) {
 
         if (!paused) {
             if (santaDiedTime === 0) {
-                const halfOfSanta = santaPerson.width / 2;
-                let speed = lastKeybEvent.shiftKey ? 21 : 7;
-                let turn = 0;
-                if (lastKeybEvent.key === "ArrowRight") {
-                    turn = 1;
-                } else if (lastKeybEvent.key === "ArrowLeft") {
-                    turn = -1;
-                }
-                if (turn !== 0) {
-                    speed *= turn;
-                    santaPerson.scale.set(-turn, 1);
-                    let rightCorellation = Math.min(santaPerson.x + speed, APP_WIDTH - halfOfSanta);
-                    santaPerson.x = Math.max(rightCorellation, halfOfSanta);
-                }
-
-                for (const index in bricksContainer.children) {
-                    const child = bricksContainer.children[index];
-                    if (child.y > APP_HEIGHT) {
-                        if (Math.abs(child.x - santaPerson.x) < 100) {
-                            santaDiedTime = Date.now();
-
-                        }
-                        bricksContainer.children.splice(index, 1);
-                    } else {
-                        child.y += 1;
-                        child.rotation += 0.05 * delta;
-                    }
-                }
-                background.tilePosition.y += 1;
-                const newBricks = generateBricks();
-                for (const key in newBricks) {
-                    if (newBricks.hasOwnProperty(key)) {
-                        const brick = newBricks[key];
-                        bricksContainer.addChild(brick);
-                    }
-                }
-
-            deltaUdpate = Date.now() - timeOfLastPartialUpdate;
-            if (
-                startGameTimestamp &&
-                (deltaUdpate > 250 || timeOfLastPartialUpdate === 0)
-            ) {
-                // TODO add cheap checks here
-                const secondsLeft = Math.ceil(
-                    GAME_COUNTDOWN_SECONDS + (startGameTimestamp - Date.now()) / 1000
-                );
-                if (secondsLeft <= 0 && timeOfLastPartialUpdate !== 0) {
-                    finishGame();
-                } else {
-                    const minutes = Math.floor(secondsLeft/60);
-                    const seconds = secondsLeft % 60;
-                    document.getElementById("minutes").innerHTML = `${minutes}:`;
-                    document.getElementById("seconds").innerHTML = `0${seconds}`.slice(
-                        -2
-                    );
-                    timeOfLastPartialUpdate = Date.now();
+                updateKeys(santaPerson);
+                updateAnimations(santaPerson, bricksContainer, background);
+                updateBricks(bricksContainer);
+                updateGameStatus();
+            } else {
+                if ((Date.now() - santaDiedTime) > 3000) {
+                    app.ticker.remove(update);
+                    gameOver(santaPerson, bricksContainer, background);
                 }
             }
+            updateSantaAnimation(santaPerson, delta);
         }
-        else{
-            if ((Date.now() - santaDiedTime) > 3000){
-                app.ticker.remove(update);
-                gameOver(santaPerson, bricksContainer, background);
-            }
-        }
-
-
-
-        let delta_santa = Date.now() - start_generation_time;
-        const santaFrame = (delta_santa / 1000 / SANTA_FRAMES_PER_SECOND - 1);
-        if (santaDiedTime !== 0){
-            santaPerson.tilePosition.x = getFrameX(SANTA_WIDTH, Math.round(53 - santaFrame % 2));
-            santaPerson.tilePosition.y = getFrameY(SANTA_HEIGHT, Math.round( 53 - santaFrame % 2));
-        }
-        else{
-            santaPerson.tilePosition.x = getFrameX(SANTA_WIDTH, Math.round(santaFrame % 50));
-            santaPerson.tilePosition.y = getFrameY(SANTA_HEIGHT, Math.round(santaFrame % 50));
-        }
-
-
-
-  };
+    }
 }
 
 let deltaUdpate = 0;

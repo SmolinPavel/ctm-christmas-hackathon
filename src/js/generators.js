70d29f226905ebd0135208163eb663736c5d0cfa
AlenaBorikova <39047199+AlenaBorikova@users.noreply.github.com>
2018-12-20 13:45:49 +0300

    generate bomb and axe

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,20 +1,8 @@
 function generateBricks() {
-    const bricksContainer = new PIXI.Container();
-    for (let rowIndex = 0; rowIndex < brickRowCount; rowIndex++){
-        for (let colIndex = 0; colIndex < brickColumnCount; colIndex++){
-            const brickContainer = new PIXI.Graphics();
-            brickContainer.lineStyle(2, 0x000000, 1);
-            brickContainer.beginFill(0x0095DD, 0.25);
-            brickContainer.drawRect(colIndex * (brickWidth + brickPadding) + brickOffsetLeft, rowIndex * (brickHeight + brickPadding) + brickOffsetTop, brickWidth, brickHeight, 15);
-            brickContainer.endFill();
-            bricksContainer.addChild(brickContainer);
-        }
-    }
-    const texture = PIXI.Texture.fromImage('./assets/bomb.png');
-    const bomb = new PIXI.Sprite(texture);
-    bomb.alpha = 0.1;
-    bricksContainer.addChild(bomb);
-    return bricksContainer;
+  const bricksContainer = new PIXI.Container();
+  bricksContainer.addChild(generateBomb());
+  bricksContainer.addChild(generateAxe());
+  return bricksContainer;
 }
 
 function generatePaddle() {
@@ -40,4 +28,14 @@ function generatePaddle() {
   //   }
 
   return paddle;
+}
+
+function generateBomb() {
+  const texture = PIXI.Texture.fromImage('./assets/bomb.png');
+  return new PIXI.Sprite(texture);
+}
+
+function generateAxe() {
+  const texture = PIXI.Texture.fromImage('./assets/axe.png');
+  return new PIXI.Sprite(texture);
 }
